d4103c6f7b8b12c88dc5d8584705351d52cbab13
tanishqiiitd <132388410+tanishqiiitd@users.noreply.github.com>
2024-01-11 23:33:11 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,27 +1,21 @@
 package assignment1;
 
 public class Book {
-    String name;
-    String author;
-    int id;
-    boolean status;
-    int seconds;
+    private String name;
+    private String author;
+    private int id;
+    private boolean status;
+    private int seconds;
 
-    Book (String name, String author, int id,boolean status, int seconds) {
-
+    public Book(String name, String author, int id, boolean status, int seconds) {
         this.name = name;
         this.author = author;
         this.id = id;
-        this.status = status;    //true = available , false = taken
+        this.status = status;    // true = available, false = taken
         this.seconds = seconds;
     }
 
-    String getname() {
+    public String getName() {
         return name;
     }
-
-
-
-
-    
 }
